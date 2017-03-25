37b62cfacf29ca8df21f9ccc29f5ae49ce189598
SARA SIMON <smbsimon@gmail.com>
2017-03-25 11:09:50 -0400

    move returned text into an index template

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'how about a game of tic tac toe?'
+    return render_template('index.html')
