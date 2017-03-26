43aa95f37f17fd7a95a3d42445c5ace538e07a75
SARA SIMON <smbsimon@gmail.com>
2017-03-25 21:47:10 -0400

    refactor board format into a list of lists; remove init function for now

--- board.py
+++ board.py
@@ -1,16 +1,15 @@
 class Board():
 
-    def __init__(self, first_row, second_row, third_row):
-        """Initialize board with three rows"""
-        self.first_row = first_row
-        self.second_row = second_row
-        self.third_row = third_row
-
     def draw_board(self):
         """Create a new instance of the board"""
-        print (self.first_row + '\n' + self.second_row + '\n' + self.third_row)
+        board = [
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+        ]
+        print board
 
-new_board = Board('[_]' * 3, '[_]' * 3, '[_]' * 3)
+new_board = Board()
 new_board.draw_board()
 
 
@@ -25,4 +24,3 @@ new_board.draw_board()
 
 # board might need more specific breakdowns:
     # center, corners, sides
-    # board construction might be better off in a list? that way i can query & insert by index...
