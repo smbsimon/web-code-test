56499e50813f1464958dadde245ebe033f3a819c
SARA SIMON <smbsimon@gmail.com>
2017-03-28 22:49:45 -0400

    add first set of raw_input instructions

--- play.py
+++ play.py
@@ -3,6 +3,19 @@ from board import Board
 class PlayGame():
     """Code for executing a run of the game"""
 
+    def start_game(self):
+        """Creates a new instance of Tic Tac Toe"""
+        print "Welcome to Tic Tac Toe. Are you ready to (p)lay or do you need some (h)elp?\n"
+        instructions = raw_input()
+
+        if instructions == "p":
+            print "All right! You make the first move. Enter the index number of your choice. The board ranges from 1-9, upper left to bottom right."
+            new_board = Board()
+            new_board.draw_board()
+        elif instructions == "h":
+            print "You really don't know how to play Tic Tac Toe?"
+        else:
+            print "Sorry. I didn't understand that."
 
     def can_win():
         """Show options for winning combinations"""
@@ -18,12 +31,16 @@ class PlayGame():
         ]
         return winning_combos
 
+
 if __name__ == "__main__":
-    new_board = Board()
-    new_board.draw_board()
+    tictactoe = PlayGame()
+    tictactoe.start_game()
 
 
 # up next: implementation of game logic
+
+# draw_board() shouldn't be called within the instructions if statement
+# users should be able to call for instructions at any point without losing state of the board
 
 # ask human player for first move input
 # for each machine move, check first to see if machine can win:
